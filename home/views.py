b863b2cfc2a625f4caba5ab7b020a0e37c756a2c
Prabhakar Mishra <prabhakarmishra222@gmail.com>
2023-01-30 23:58:35 +0530

    implemented pagination on home page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from .models import *
+from django.core.paginator import Paginator
 # Create your views here.
 def home(request):
-    return render(request, 'home.html' , context={"articles": Article.objects.all().order_by('-created_at')})
+    context = {}
+    articles_obj = Article.objects.all()
+    page = request.GET.get('page', 1)
+    p = Paginator(articles_obj,15)
+    try:
+        articles_obj = p.page(page)
+    except :
+        articles_obj = p.page(1)
+    return render(request, 'home.html' , context={"articles": articles_obj})
+        
+    
