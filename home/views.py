a3637678293b7f535a9fe2e2b9082be677c757f8
Prabhakar Mishra <prabhakarmishra222@gmail.com>
2023-02-06 04:06:28 +0530

    Commiting changes

--- home/views.py
+++ home/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from .models import *
 from django.core.paginator import Paginator
+
+
 # Create your views here.
 def home(request):
     context = {}
     articles_obj = Article.objects.all()
-    page = request.GET.get('page', 1)
-    p = Paginator(articles_obj,15)
+    page = request.GET.get("page", 1)
+    p = Paginator(articles_obj, 15)
     try:
         articles_obj = p.page(page)
-    except :
+    except:
         articles_obj = p.page(1)
-    return render(request, 'home.html' , context={"articles": articles_obj, "page_obj":articles_obj})
-        
-    
+    return render(
+        request,
+        "home.html",
+        context={"articles": articles_obj, "page_obj": articles_obj},
+    )
